bac556aace224c8a9511530642d73e666fa1dab6
EEZEH01 <esamayamo@gmail.com>
2025-04-11 16:37:25 -0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-# app/app.py
-
-from app.agents import UrbanAgent
-
-if __name__ == "__main__":
-    agent = UrbanAgent()
-    user_input = input("Escribe tu mensaje: ")
-    response = agent.respond(user_input)
-    print(response)
