9c4f8c8f7592fd98c1d4d4cb5d3eb221e23bc39b
EEZEH01 <esamayamo@gmail.com>
2025-04-12 14:39:46 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
+# __init__.py
+
 from flask import Flask
-from app.api.routes import api  # Blueprint import
+from config import config  # Import the configuration from config.py
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(api, url_prefix="/api")
+
+    # Load the configuration from the config.py file
+    app.config.from_object(config)  # This applies the configuration settings to the Flask app
+
     return app
+
